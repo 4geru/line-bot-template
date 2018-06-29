dc7f367898d06f956409610e82190fa3cd49a551
a4geru <is0248vx@ed.ritsumei.ac.jp>
2018-06-29 01:53:40 +0000

    add user status

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180629015005) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "line_id"
+    t.string   "name"
+    t.string   "picture"
+    t.string   "status_id",  default: "1"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
